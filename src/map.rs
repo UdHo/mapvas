f6540ed5101f358eee72d766ccddd3477eedadaa
Udo Hoffmann <hoffmann.odu@gmail.com>
2025-03-17 17:24:51 +0100

    moving stuff

---
+++ src/map.rs
@@ -0,0 +1,8 @@
+/// Contains everything needed to handle coordinates.
+pub mod coordinates;
+/// Stuff to be send to the map client.
+pub mod map_event;
+/// The map widget.
+pub mod mapvas_egui;
+/// Map tile functionality.
+mod tile_loader;
