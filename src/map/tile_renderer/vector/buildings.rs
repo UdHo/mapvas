7b379c5300f71c1ae94331e18563dda1ddb35ff8
Udo Hoffmann <hoffmann.odu@gmail.com>
2026-01-16 13:21:49 +0100

    Repair forest rendering

---
+++ src/map/tile_renderer/vector/buildings.rs
@@ -0,0 +1,3 @@
+// Building rendering module
+// Currently handled by generic polygon rendering
+// Future: Could contain 3D building rendering, roof details, etc.
---
+++ src/map/tile_renderer/vector/places.rs
@@ -0,0 +1,3 @@
+// Place (city/town/village) rendering module
+// Currently handled by render_point_with_class in mod.rs
+// Future: Could contain place-specific styling, icon rendering, etc.
